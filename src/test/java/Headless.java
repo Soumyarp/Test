18eb274bdc5a90313ffcd62fc4a888dcfc0d37ef
Soumya Ranjan <ranjan@securax.in>
2018-09-05 15:17:06 +0530

    Headless automation

---
+++ src/test/java/Headless.java
@@ -0,0 +1,42 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Headless {
+
+	public static void main(String[] args) {
+	//	HtmlUnitDriver driver = new HtmlUnitDriver();
+		ChromeOptions option = new ChromeOptions();
+	//	option.addArguments("window-size=1400,800");
+		option.addArguments("headless");
+	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver(option);
+			
+	//driver.get("https://qa-org.securtime.in");
+		driver.get("https://securax.securtime.in");
+	System.out.println(driver.getTitle());
+	driver.findElement(By.xpath("//input[@type='email']")).clear();
+	driver.findElement(By.xpath("//input[@type='email']")).sendKeys("ranjan@securax.in");
+	
+	driver.findElement(By.xpath("//input[@type='password']")).clear();
+	driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Securax@123");
+	
+	driver.findElement(By.xpath("//button[@type='submit']")).click();
+	
+	System.out.println(driver.getTitle()+"-------"+driver.getCurrentUrl());
+//	System.out.println(driver.getCurrentUrl());
+	
+	
+	
+	
+
+	}
+	
+		
+	}
+
+
